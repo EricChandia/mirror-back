15b1ec93f126238458fc076df0dae226694ace8e
EricChandia <eric.chandia.ti@gmail.com>
2022-09-29 01:13:27 -0300

    first commit/project structure made

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express from "express";
+import "express-async-errors";
+import handleErrorsMiddleware from './middlewares/errorHandlerMiddleware';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+//app.use(router);
+app.use(handleErrorsMiddleware);
+
+export default app;
